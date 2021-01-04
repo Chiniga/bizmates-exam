71f86007ef928217e7e5f70687ace76a151cfd71
David <david.graves.2011@gmail.com>
2021-01-04 08:50:57 +0800

    Add controllers

---
+++ app/Http/Controllers/Api/LocationController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\LocationResource;
+use Illuminate\Http\Request;
+
+class LocationController extends Controller
+{
+    use LocationResource;
+}
