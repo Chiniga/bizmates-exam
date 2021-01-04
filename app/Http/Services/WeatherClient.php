ae568068eddcf4417c728c50e94d8e84e431b27f
David <david.graves.2011@gmail.com>
2021-01-04 13:13:36 +0800

    Add weather contract and client

---
+++ app/Http/Services/WeatherClient.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Services;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
+class WeatherClient implements WeatherContract
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * WeatherClient constructor
+     */
+    public function __construct()
+    {
+        $this->guzzle = new Client();
+    }
+
+    /**
+     * @param string $city
+     * @return array
+     */
+    public function request($city)
+    {
+        $params = http_build_query([
+            'q'     => $city,
+            'appid' => config('app.weather_api_key')
+        ]);
+
+        try {
+            $response = $this->guzzle->request('GET', config('app.weather_url') . '?' . $params);
+            $statusCode = $response->getStatusCode();
+            $content = json_decode($response->getBody()->getContents(), true);
+        } catch (ClientException $exception) {
+            $statusCode = $exception->getCode();
+            $content = $exception->getMessage();
+        }
+
+        return [
+            'statusCode' => $statusCode,
+            'content'    => $content
+        ];
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Http\Services\WeatherClient;
+use App\Http\Services\WeatherContract;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(WeatherContract::class, function ($app) {
+            return new WeatherClient();
+        });
     }
 
     /**
