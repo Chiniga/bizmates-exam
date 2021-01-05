c773c36f994e784fc984ef4d056e0394f692ea93
David <david.graves.2011@gmail.com>
2021-01-05 10:19:43 +0800

    Simplify services registration and remove singleton usage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,18 +9,19 @@ use App\Http\Services\LocationClient;
 use App\Http\Services\LocationContract;
 
 class AppServiceProvider extends ServiceProvider {
+
+    public $bindings = [
+        WeatherContract::class  => WeatherClient::class,
+        LocationContract::class => LocationClient::class
+    ];
+
     /**
      * Register any application services.
      *
      * @return void
      */
     public function register() {
-        $this->app->singleton(WeatherContract::class, function ($app) {
-            return new WeatherClient();
-        });
-        $this->app->singleton(LocationContract::class, function ($app) {
-            return new LocationClient();
-        });
+        //
     }
 
     /**
